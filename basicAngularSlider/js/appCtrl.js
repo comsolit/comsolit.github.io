5966071f49faf9c225f8b421a41b4667e85086fb
Lars Messmer <messmer@comsolit.ch>
2014-11-27 17:41:03 +0100

    [add] basicAngularSlider

---
+++ basicAngularSlider/js/appCtrl.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('appCtrl', appCtrl);
+
+    appCtrl.$inject = ['$scope']; 
+
+    function appCtrl($scope) {
+        $scope.images = [
+            'images/landscape1.jpg',
+            'images/landscape2.jpg',
+            'images/landscape3.jpg',
+            'images/landscape4.jpg'
+        ];
+    }
+})();
---
+++ basicAngularSlider/js/directives/imgSlider/imgSlider.js
@@ -0,0 +1,67 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('imgSlider', imgSlider);
+
+    imgSlider.$inject = ['$window'];
+    
+    function imgSlider ($window) {
+        var directive = {
+            link: link,
+            restrict: 'EA',
+            scope: {
+                images: '='
+            },
+            templateUrl: 'templates/imgSlider/imgSlider.html'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            var timeout = 5000;
+            var currentIndex = 0;
+            var skipInterval = false;
+
+            scope.isCurrentIndex = function (index) {
+                return index == currentIndex;
+            };
+
+            function nextSlide() {
+                if ((scope.images.length - 1) > currentIndex) {
+                    currentIndex++;
+                } else {
+                    currentIndex = 0;
+                }
+            }
+
+            function prevSlide() {
+                if (currentIndex == 0) {
+                    currentIndex = scope.images.length - 1;
+                } else {
+                    currentIndex--;
+                }
+            }
+
+            scope.next = function () {
+                nextSlide();
+                skipInterval = true;
+            };
+
+            scope.prev = function () {
+                prevSlide();
+                skipInterval = true;
+            };
+
+            var slide = setInterval(function () {
+                if (!skipInterval) {
+                    scope.next();
+                    scope.$apply();
+                }
+                skipInterval = false;
+
+            }, timeout);
+        }
+    }
+
+})();
